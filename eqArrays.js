8e532db34578c7815c002947c88185a6f48dd85f
Sharleen Beniamin <sharleenb16@gmail.com>
2023-01-29 18:48:52 +0000

    created function and test cases

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i=0; i < array1.length; i++)
+    if (array1[i] !== array2[i]){
+      return false
+    } 
+    return true
+  }
+  
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
