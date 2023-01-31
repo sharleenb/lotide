3677c04276bc4b6fbd0abcb8ffc1c63b7d81efec
Sharleen Beniamin <sharleenb16@gmail.com>
2023-01-31 23:51:10 +0000

    created function and testing

---
+++ findKeyByValue.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let match = true
+  for (let i=0; i < array1.length; i++)
+    if (array1[i] !== array2[i]){
+      match = false
+    } 
+    if (match === false) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
+    } else {
+      console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+    }
+  }
+
+  const findKeyByValue = function(bestTVShowsByGenre, value){
+    let result = undefined
+    for (const key in bestTVShowsByGenre) {
+      if (bestTVShowsByGenre[key] === value) {
+        result = key
+      }
+    }
+    return result
+  }
+  const bestTVShowsByGenre = { 
+    sci_fi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire"
+  };
+
+  console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+  
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
