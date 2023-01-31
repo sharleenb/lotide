24c06b254b67eb3626ff42f2a051bd0aa0e90bc7
Sharleen Beniamin <sharleenb16@gmail.com>
2023-01-31 23:12:18 +0000

    added for loop to add indices of each letter

--- letterPositions.js
+++ letterPositions.js
@@ -6,14 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const assertArraysEqual = function(array1, array2) {
   let match = true
   for (let i=0; i < array1.length; i++)
@@ -30,8 +22,18 @@ const assertArraysEqual = function(array1, array2) {
   
 const letterPositions = function(sentence) {
   const results = {};
-  
+  for (let i = 0; i < sentence.length; i++){
+    if (sentence[i] !== ' '){
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i)
+      } else {
+        results[sentence[i]] = [i]
+      }
+    }
+    }
   return results;
 };
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+console.log(letterPositions("lighthouse in the house"))
+
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
