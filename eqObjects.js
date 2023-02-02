32a46c3e78054eca4d1e82a9a5a37900f3f068cf
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-02 15:48:27 +0000

    removed test console logs

--- eqObjects.js
+++ eqObjects.js
@@ -18,8 +18,6 @@ const eqArrays = function(array1, array2) {
 const eqObjects = function(object1, object2) {
   const objArray1 = Object.keys(object1);
   const objArray2 = Object.keys(object2);
-  console.log(objArray1);
-  console.log(objArray2);
   let result = false;
   if (objArray1.length === objArray2.length) {
     for (const key of objArray1) {
