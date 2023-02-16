003c9fa6470f1a5006e9e0487152d1e9fb857103
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-16 01:42:09 +0000

    refactored code to remove tests

--- tail.js
+++ tail.js
@@ -1,18 +1,9 @@
-const assertEqual = function(actual, expected) {
-  const stringActual = actual.toString();
-  const stringExpected = expected.toString();
-  if (stringActual === stringExpected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const tail = function(input) {
   return input.slice(1);
 };
 
-const input = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(input));
-assertEqual(tail(input), ["Lighthouse", "Labs"]);
+module.exports = tail;
+
 
