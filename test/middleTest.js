1692c31e348c183fececc40c7684fc59a5362d1a
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-16 03:40:18 +0000

    changed assertEqual tests to mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,8 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]))
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,9 @@
-const assertEqual = require('../assertEqual'); 
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const input = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(input), ["Lighthouse", "Labs"]);
+describe("#tail", () => {
+  it("returns 'Lighthouse', 'Labs' for 'Yo Yo', 'Lighthouse', 'Labs'", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
 
