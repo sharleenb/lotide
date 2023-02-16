46c577aaa84e2e76aca911eefb312ca5d4869617
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-16 03:17:10 +0000

    assert arrays refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
-  let match = true
+  let result = `✅✅✅ Assertion Passed: ${array1} === ${array2}`
   for (let i=0; i < array1.length; i++)
     if (array1[i] !== array2[i]){
-      match = false
+      result = `🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`;
     } 
-    if (match === false) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`)
-    } else {
-      console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
-    }
+    return result
+
   }
-console.log(assertArraysEqual([1,2,'3'], [1,2,3]))
+console.log(assertArraysEqual([1,2,3], [1,2,3]))
+
+module.exports = assertArraysEqual;
