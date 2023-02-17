729ab547d76d10785fe6a73af741bde86c186183
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-17 23:38:20 +0000

    modularize code, remove dead code, and format code

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const assertArraysEqual = require('./assertArraysEqual');
 
 const map = function(array, callback) {
   const results = [];
@@ -8,33 +8,5 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
 
-console.log(map());
-
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-const assertArraysEqual = function(array1, array2) {
-  let match = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      match = false;
-    }
-    if (match === false) {
-      console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    } else {
-      console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-    }
-  }
-};
-  
-  
-console.log(assertArraysEqual(map(), [ 'g', 'c', 't', 'm', 't' ]));
 
