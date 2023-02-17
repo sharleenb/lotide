cbf036dcec4b97a15c8dee8d5f5ccbf38d904b99
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-17 23:38:59 +0000

    modularize code, remove dead code, and format code

--- without.js
+++ without.js
@@ -1,26 +1,20 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const without = function(source, itemsToRemove){
-  const newArray = []
+const without = function(source, itemsToRemove) {
+  const newArray = [];
   for (let i = 0; i < source.length; i++) {
-    let match = false
+    let match = false;
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (itemsToRemove[j] === source[i]) {
-        match = true
+        match = true;
       }
     }
     if (match == false) {
-      newArray.push(source[i])
+      newArray.push(source[i]);
     }
   }
-  return newArray
+  return newArray;
 }
 
-console.log(without([1, 2, 3], [1]))
+
 
