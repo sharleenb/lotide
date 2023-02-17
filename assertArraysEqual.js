4b115b9c0e1e8ce187016a9bc8facbd39cae2a51
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-17 23:36:52 +0000

    modularize code, remove dead code, and format code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,14 +2,13 @@
 const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
-  let result = `✅✅✅ Assertion Passed: ${array1} === ${array2}`
-  for (let i=0; i < array1.length; i++)
+  let result = `✅✅✅ Assertion Passed: ${array1} === ${array2}`;
+  for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]){
       result = `🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`;
     } 
-    return result
-
   }
-console.log(assertArraysEqual([1,2,3], [1,2,3]))
-
+    return result
+};
+  
 module.exports = assertArraysEqual;
