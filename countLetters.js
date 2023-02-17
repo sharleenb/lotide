2cbe27619dac42e2233d2ccfd72394d85cac76af
Sharleen Beniamin <sharleenb16@gmail.com>
2023-02-17 23:37:18 +0000

    modularize code, remove dead code, and format code

--- countLetters.js
+++ countLetters.js
@@ -1,35 +1,15 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const countLetters = function(input){
+const countLetters = function(input) {
   const result = {};
   for (const letter of input) {
-    if (letter !== ' '){
+    if (letter !== ' ') {
       if(result[letter]) {
-        result[letter] += 1
+        result[letter] += 1;
       } else {
-        result[letter] = 1
+        result[letter] = 1;
       }
     }
   }
   return result
-}
-
-const result1 = countLetters("lighthouse in the house")
-
-//assertEqual((result1), {l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,})
-assertEqual((result1["l"]), 1)
-assertEqual((result1["i"]), 2)
-assertEqual((result1["g"]), 1)
-assertEqual((result1["h"]), 4)
-assertEqual((result1["t"]), 2)
-assertEqual((result1["o"]), 2)
-assertEqual((result1["u"]), 2)
-assertEqual((result1["s"]), 2)
-assertEqual((result1["e"]), 3)
-assertEqual((result1["n"]), 1)
+};
